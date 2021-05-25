6078e2e0299071a18cfcaae234b444126738cf98
EkkiRu <63056889+EkkiRu@users.noreply.github.com>
2021-05-26 01:08:47 +0600

    First night
     First night

---
+++ Thesis/Thesis/AuthorizationWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Thesis
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FormWithClasses(object sender, RoutedEventArgs e)
+        {
+            var group = Group.Text;
+            var fullName = FullName.Text;
+            bool isCheck = false;
+            if (group == "")
+            {
+                ErrorGroup.Content = "Введите группу";
+                isCheck = true;
+            }
+            else
+                ErrorGroup.Content = "";
+            if (fullName == "")
+            {
+                ErrorFullName.Content = "Введите ФИО";
+                isCheck = true;
+            }
+            else
+                ErrorFullName.Content = "";
+            if (isCheck)
+                return;
+            MaterialsWindow mainWindow = new MaterialsWindow(fullName,group,DateTime.Now);
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Thesis/Thesis/Constant/Constant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thesis.Constant
+{
+    public static class Constant
+    {
+        public static List<string> Topic { get; set; } = new List<string> { "ОҚУ - ӘДІСТЕМЕЛІК КЕШЕН"};
+        public static List<string> Content { get; set; } = new List<string> { "Теоретический материал", "Практические задания", "Глоссарий" };
+    }
+}
